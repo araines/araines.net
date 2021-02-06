99cf8a074891b7a54ca48215ec20575c2a8e79fd
Andy Raines <andrew.raines@gmail.com>
2021-02-06 12:39:03 +0000

    Start configuration

--- blog.config.js
+++ blog.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-  url: "https://nextjs-mdx-blog-starter.vercel.app/",
-  title: "Next.js MDX Blog Starter",
-  subtitle: "Content authoring with MDX. Powered by Next.js.", // longDescription : appears in landing page meta description
-  description:
-    "Next.js MDX Blog Starter for building blogs with Next.js and MDX, including Theme UI Component Design System, Vercel Deployment and more.",
-  author: "John Polacek",
+  url: "https://araines.net/",
+  title: "Andy Raines",
+  subtitle: "Software engineer, habitual tinkerer, gamer", // longDescription : appears in landing page meta description
+  description: "Personal blog for Andy Raines.  Software engineering, technology tinkering and more.",
+  author: "Andy Raines",
   analytics: "", // put your google analytics code here to add tracking
-  shareImage: "https://nextjs-mdx-blog-starter.vercel.app/img/screenshot.png",
-  shareImageAlt: "Next.js MDX Blog Starter Screenshot",
+  shareImage: "https://araines.net/img/screenshot.png",
+  shareImageAlt: "Andy Raines",
   postsPerPage: 8,
   showDate: true, // if you would like the date to appear on the blog posts
-  disqus: "", // your disqus shortname, remove if you don't use disqus
+//  disqus: "", // your disqus shortname, remove if you don't use disqus
 }
