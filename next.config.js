848b6aa3ec39917c86bd8d3e50f1b5da6c3287b8
Andy Raines <andrew.raines@gmail.com>
2022-01-31 20:44:17 +0000

    Fix next.js issue

--- next.config.js
+++ next.config.js
@@ -3,12 +3,10 @@ const withMDX = require("@next/mdx")({
 })
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "md", "mdx"],
+  webpack5: true,
   webpack: (config, { isServer }) => {
-    // Fixes npm packages (mdx) that depend on `fs` module
     if (!isServer) {
-      config.node = {
-        fs: "empty",
-      }
+      config.resolve.fallback.fs = false
     }
     return config
   },
